2d0f085eaba88c78872c2c6e50765ac550d88812
Nikam-Mayuri <150147459+Nikam-Mayuri@users.noreply.github.com>
2025-08-04 02:10:29 +0530

    Add files via upload

---
+++ Assignment 3/Palindrome.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main() 
+{
+    int n, reversed = 0, temp, digit;
+
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    // Reverse the number
+    while (n > 0)
+	 {
+        digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n = n / 10;
+    }
+
+    
+    if (temp == reversed) 
+	{
+        printf("Palindrome\n");
+    } else {
+        printf("Not Palindrome\n");
+    }
+}
