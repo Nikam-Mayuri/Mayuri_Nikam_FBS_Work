9cdcc0d3a1e134e5d37fa35aff4a1903457b6419
Nikam-Mayuri <150147459+Nikam-Mayuri@users.noreply.github.com>
2025-08-04 01:58:24 +0530

    Add files via upload

---
+++ Assignment 3/Strong number.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void main() 
+{
+    int num, temp, digit, sum = 0, i, fact;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    // Loop to extract each digit
+    while (num > 0) 
+	{
+        digit = num % 10;
+
+        // Calculate factorial of the digit
+        fact = 1;
+        i = 1;
+        while (i <= digit) 
+		{
+            fact *= i;
+            i++;
+        }
+
+        sum =sum+fact;
+        num = num / 10;
+    }
+
+    
+    if (sum == temp) 
+	{
+        printf("%d is a Strong Number.\n", temp);
+    } else {
+        printf("%d is not a Strong Number.\n", temp);
+    }
+}
