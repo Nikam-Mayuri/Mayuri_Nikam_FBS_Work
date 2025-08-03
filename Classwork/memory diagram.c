8cae1f56866b730de8a9355eee235a5a0a9f10ac
Nikam-Mayuri <150147459+Nikam-Mayuri@users.noreply.github.com>
2025-08-03 15:01:52 +0530

    Add files via upload

---
+++ Classwork/memory diagram.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int a=5,b=7,x;
+	for(int i=2;i<5;i++)
+	{
+		x=a+b;
+		for(int j=1;j<=i;j++)
+		{
+a=a+i;
+b=b+j;
+
+		}
+		printf("\n%d",x);
+		printf("\n%d",a);
+		printf("\n%d",b);
+			
+	}
+
+}
