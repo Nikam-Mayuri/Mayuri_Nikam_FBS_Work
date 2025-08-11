ba93f4456105669953cbe1986001b239cf996401
Nikam-Mayuri <150147459+Nikam-Mayuri@users.noreply.github.com>
2025-08-11 23:40:42 +0530

    Add files via upload

---
+++ Assignment 7/sum of start and end pointer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void findSum(int*, int*); // Function declaration
+
+int main() {
+    int start, end;
+    printf("Enter start number: ");
+    scanf("%d", &start);
+    printf("Enter end number: ");
+    scanf("%d", &end);
+
+    findSum(&start, &end);  // Function call
+    return 0;
+}
+
+void findSum(int *start, int *end) { // Function definition
+    int sum = 0;
+    for (int i = *start; i <= *end; i++) {
+        sum += i;
+    }
+
+    printf("Sum from %d to %d is: %d\n", *start, *end, sum);
+}
